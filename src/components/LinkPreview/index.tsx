cb81e1ef6a64380daa607b360e33600e9cf7b2ad
Chi Kei Chan <chikeichan@gmail.com>
2020-12-01 16:54:10 -0800

    preview: add youtube, video, and image

--- src/components/LinkPreview/index.tsx
+++ src/components/LinkPreview/index.tsx
@@ -2,10 +2,6 @@ import React, {ReactElement, useEffect, useState} from "react";
 import classNames from "classnames";
 import {INDEXER_API} from "../../utils/api";
 import "./link-preview.scss";
-import {url} from "inspector";
-import Icon from "../Icon";
-import {Simulate} from "react-dom/test-utils";
-import playing = Simulate.playing;
 
 type LinkPreviewProps = {
   className?: string;
@@ -13,7 +9,11 @@ type LinkPreviewProps = {
   onOpenLink: (url: string) => void;
 }
 
-const CACHE: any = {};
+const HTML_CACHE: any = {};
+const YT_CACHE: any = {};
+const VID_CACHE: any = {};
+const IMG_CACHE: any = {};
+const URL_LOADING: any = {};
 
 export default function LinkPreview(props: LinkPreviewProps): ReactElement {
   const [htmlProps, setHtmlProps] = useState<{
@@ -33,6 +33,17 @@ export default function LinkPreview(props: LinkPreviewProps): ReactElement {
     description: string;
     images: string[];
   } | null>(null);
+  const [videoProps, setVideoProps] = useState<{
+    videoUrl: string;
+    title: string;
+    description: string;
+    images: string[];
+  } | null>(null);
+  const [imageProps, setImageProps] = useState<{
+    title: string;
+    description: string;
+    imageUrl: string;
+  }| null>(null);
 
   const [host, setHost] = useState('');
   const [loading, setLoading] = useState(true);
@@ -40,39 +51,112 @@ export default function LinkPreview(props: LinkPreviewProps): ReactElement {
   useEffect(() => {
     (async function onLinkPreviewMount() {
       try {
-        if (CACHE[props.url]) {
-          return setHtmlProps(CACHE[props.url]);
+        if (URL_LOADING[props.url]) {
+          return;
+        }
+
+        if (VID_CACHE[props.url]) {
+          return setVideoProps(VID_CACHE[props.url]);
+        }
+
+        if (IMG_CACHE[props.url]) {
+          return setImageProps(IMG_CACHE[props.url]);
+        }
+
+        if (HTML_CACHE[props.url]) {
+          return setHtmlProps(HTML_CACHE[props.url]);
+        }
+
+        if (YT_CACHE[props.url]) {
+          return setYoutubeProps(YT_CACHE[props.url]);
         }
 
         const url = new URL(props.url);
+
         setHost(url.hostname);
+
+        URL_LOADING[props.url] = true;
         const resp = await fetch(`${INDEXER_API}/preview?url=${encodeURI(props.url)}`);
         const json = await resp.json();
-
-        if (json.mediaType === 'website') {
-          setHtmlProps(json);
-          setYoutubeProps(null);
-          CACHE[props.url] = json;
-          return;
-        }
 
         if (json.siteName === 'YouTube') {
           const regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
           const match = props.url.match(regExp);
           if (match && match[2].length == 11) {
-            console.log(json);
-            setYoutubeProps({
+            setHtmlProps(null);
+            setImageProps(null);
+            setVideoProps(null);
+            YT_CACHE[props.url] = {
               title: json.title,
               description: json.description,
               videoId: match[2],
               images: json.images,
-            });
-            setHtmlProps(null);
+            };
+            setYoutubeProps(YT_CACHE[props.url]);
             return;
           }
         }
+
+        if (json.mediaType === 'video.other' && json.siteName === 'Imgur') {
+          setHtmlProps(null);
+          setYoutubeProps(null);
+          setImageProps(null);
+          VID_CACHE[props.url] = {
+            videoUrl: json.videos[0]?.url,
+            title: json.title,
+            description: json.description,
+            images: json.images,
+          };
+          setVideoProps(VID_CACHE[props.url]);
+          return;
+        }
+
+        if (json.mediaType === 'image') {
+          setHtmlProps(null);
+          setYoutubeProps(null);
+          setVideoProps(null);
+          IMG_CACHE[props.url] = {
+            title: json.title,
+            description: json.description,
+            imageUrl: json.url,
+          };
+          setImageProps(IMG_CACHE[props.url]);
+          return;
+        }
+
+        if (json.contentType === 'application/pdf') {
+          setHtmlProps({
+            ...json,
+            url: json.url,
+            title: json.contentType,
+            siteName: url.host,
+            images: [],
+          });
+          setYoutubeProps(null);
+          setImageProps(null);
+          setVideoProps(null);
+          HTML_CACHE[props.url] = {
+            ...json,
+            url: json.url,
+            title: json.contentType,
+            siteName: url.host,
+            images: [],
+          };
+          return;
+        }
+
+        if (json) {
+          setHtmlProps(json);
+          setYoutubeProps(null);
+          setImageProps(null);
+          setVideoProps(null);
+          HTML_CACHE[props.url] = json;
+          return;
+        }
+
       } finally {
         setLoading(false);
+        URL_LOADING[props.url] = false;
       }
     })();
   }, [props.url]);
@@ -82,10 +166,10 @@ export default function LinkPreview(props: LinkPreviewProps): ReactElement {
   const body = (
     <div className="link-preview__body">
       <div className="link-preview__title">
-        {htmlProps?.title || youtubeProps?.title}
+        {htmlProps?.title || youtubeProps?.title || videoProps?.title}
       </div>
       <div className="link-preview__description">
-        {htmlProps?.description || youtubeProps?.description}
+        {htmlProps?.description || youtubeProps?.description || videoProps?.description}
       </div>
       <div className="link-preview__host">
         {host}
@@ -100,6 +184,8 @@ export default function LinkPreview(props: LinkPreviewProps): ReactElement {
         e.stopPropagation();
         e.preventDefault();
         if (youtubeProps && !playingVideo) {
+          setPlaying(true);
+        } else if (videoProps && !playingVideo) {
           setPlaying(true);
         } else {
           props.onOpenLink!(props.url);
@@ -126,8 +212,10 @@ export default function LinkPreview(props: LinkPreviewProps): ReactElement {
           ? (
             <iframe
               src={`https://www.youtube.com/embed/${youtubeProps?.videoId}`}
-              width={640}
+              width="100%"
               height={360}
+              loading="lazy"
+              referrerPolicy="same-origin"
             />
           )
           : (
@@ -141,7 +229,42 @@ export default function LinkPreview(props: LinkPreviewProps): ReactElement {
           )
       }
       {
-        youtubeProps && playingVideo
+        !videoProps?.videoUrl ? null : playingVideo
+          ? (
+            <video
+              src={videoProps?.videoUrl}
+              width="100%"
+              height={360}
+              autoPlay
+              controls
+            />
+          )
+          : (
+            <div
+              className="link-preview__image"
+              style={{
+                backgroundImage: `url(${videoProps?.images[0]})`,
+              }}
+            />
+          )
+      }
+      {
+        imageProps && (
+          <div
+            className="link-preview__image-wrapper"
+          >
+            <div
+              className="link-preview__image-wrapper__image-bg"
+            />
+            <div
+              className="link-preview__image-wrapper__image"
+              style={{backgroundImage: `url(${imageProps.imageUrl})`}}
+            />
+          </div>
+        )
+      }
+      {
+        playingVideo || imageProps
           ? null
           : body
       }
